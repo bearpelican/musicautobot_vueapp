0e94084ebbc342aae3065fde57111775e3d1f6dc
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-03-22 02:58:18 -0700

    Creating sequencer.js so we can pass in midi file

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,14 @@
       <router-link to="/api">API Sample</router-link> |
       <router-link to="/midi">Midi</router-link>
     </div>
-    <div id="logos">
+    <!-- <div id="logos">
       <p>
         <img src="@/assets/vue-logo.png">
         <span id="plus">+</span>
         <img src="@/assets/flask-logo.png">
       </p>
-    </div>
+    </div> -->
     <router-view/>
-
-    <!-- FORK Me -->
-    <a href="https://github.com/gtalarico/flask-vuejs-template"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
-
   </div>
 </template>
 
