fe750a6f335fe9fd587be4d2c53e1f33b068bdf2
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-03-22 02:36:39 -0700

    fixing heartbeat export error

--- src/views/Midi.vue
+++ src/views/Midi.vue
@@ -1,21 +1,63 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + Flask App"/>
+    <div id='editor'>
+      <div id='score'>
+        <div id='pitch-lines'></div>
+        <div id='bar-lines'></div>
+        <div id='beat-lines'></div>
+        <div id='sixteenth-lines'></div>
+        <div id='notes'></div>
+        <div id='parts'></div>
+        <div id='playhead'>
+            <div id='playhead-line'></div>
+        </div>
+      </div>
+    </div>
+    <div id='controls'>
+      <input type='button' id='play' value='play' />
+      <input type='button' id='stop' value='stop' />
+
+      <div id='time-bars-beats'></div>
+
+      <div class='pipe'>|</div>
+      <div id='time-seconds'></div>
+
+      <div class='pipe'>|</div>
+      <div id='mouse-x'>0</div>
+
+      <div class='pipe'>|</div>
+      <div id='mouse-y'>0</div>
+
+      <div class='pipe'>|</div>
+      <input type='button' id='first' value='<<' />
+      <input type='button' id='prev' value='<' />
+      <div id='page-numbers'>page 0 of 0</div>
+      <input type='button' id='next' value='>' />
+      <input type='button' id='last' value='>>' />
+
+      <div class='pipe'>|</div>
+      <div>
+        <input type='range' id='scale-slider'/>
+        <label for='scale-slider' id='scale-label'>#bars 16</label>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-// var heartbeat = require('../../public/js/heartbeat.js')
-// var main = require('../../public/js/main.js')
-require('../../public/js/heartbeat.js')
-require('../../public/js/main.js')
+import '../../public/js/heartbeat.js' // import heartbeat library
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  mounted () {
+    // import '../../public/js/main.js'
+    require('../../public/js/main.js') // import needs to be at top level, so using require
   }
 }
 </script>
