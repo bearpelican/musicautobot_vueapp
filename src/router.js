09af13f850eff1e675194e2612f758c3613ebe51
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-03-22 01:36:26 -0700

    Added key_editor to template

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Api from './views/Api.vue'
+import Midi from './views/Midi.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/api',
       name: 'api',
       component: Api
+    },
+    {
+      path: '/midi',
+      name: 'midi',
+      component: Midi
     }
   ]
 })
