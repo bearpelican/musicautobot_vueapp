db2bb25b6366d4e2b27218ba33f02111e58f31f6
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-03-25 16:21:37 -0700

    Adding bootstrap-vuew package

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// app.js
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,6 +12,7 @@ import store from './store'
 import './filters'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
