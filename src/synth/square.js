f07d59718002361a4d81ea89919a590587b86dda
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-03-27 13:48:06 -0700

    Adding google material and getting buttons to work

--- src/synth/square.js
+++ src/synth/square.js
@@ -1,50 +1,50 @@
-import SynthBase from "@/synth/synthBase";
+import SynthBase from '@/synth/synthBase'
 
 export class Note {
-  constructor(osc, frequency, time = null, onDestroyed = null) {
-    this.osc = osc;
-    this.osc.frequency.value = frequency;
-    this.osc.start();
+  constructor (osc, frequency, time = null, onDestroyed = null) {
+    this.osc = osc
+    this.osc.frequency.value = frequency
+    this.osc.start()
     if (time !== null) {
       setTimeout(() => {
-        this.osc.stop();
+        this.osc.stop()
         if (onDestroyed) {
-          onDestroyed();
+          onDestroyed()
         }
-      }, time);
+      }, time)
     }
   }
 }
 export default class SquareSynth extends SynthBase {
-  audioCtx = new AudioContext();
-  gainNode = this.audioCtx.createGain();
-  notes = [];
-  constructor() {
-    super();
-    this.gainNode.gain.value = 0.1;
-    this.gainNode.connect(this.audioCtx.destination);
+  constructor () {
+    super()
+    this.audioCtx = new AudioContext()
+    this.gainNode = this.audioCtx.createGain()
+    this.notes = []
+    this.gainNode.gain.value = 0.1
+    this.gainNode.connect(this.audioCtx.destination)
   }
-  createOsc() {
-    const osc = this.audioCtx.createOscillator();
-    osc.connect(this.gainNode);
-    osc.type = "square";
-    return osc;
+  createOsc () {
+    const osc = this.audioCtx.createOscillator()
+    osc.connect(this.gainNode)
+    osc.type = 'square'
+    return osc
   }
-  play(frequency, time = null) {
-    let note;
+  play (frequency, time = null) {
+    let note
     if (time) {
       note = new Note(this.createOsc(), frequency, time, () => {
-        this.notes.splice(this.notes.indexOf(note), 1);
-      });
+        this.notes.splice(this.notes.indexOf(note), 1)
+      })
     } else {
-      note = new Note(this.createOsc(), frequency);
+      note = new Note(this.createOsc(), frequency)
     }
-    this.notes.push(note);
+    this.notes.push(note)
   }
-  stop() {
+  stop () {
     for (let note of this.notes) {
-      note.osc.stop();
+      note.osc.stop()
     }
-    this.notes = [];
+    this.notes = []
   }
 }
