73494d85617e1c2b7e9ac466227a07e745817e6b
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-03-28 17:48:56 -0700

    Updating home page with links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,29 +2,29 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
+      This project uses recent advances in text generation
+      <a href="https://arxiv.org/abs/1901.02860" target="_blank">source code</a> to generate pop music from midi files.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Source Code</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
+      <li><a href="https://github.com/bearpelican/vue_midi_generator" target="_blank">Vue/Flask Web App (this site)</a></li>
+      <li><a href="https://github.com/bearpelican/midi_generator" target="_blank">Midi model training project</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Data Sources</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <li><a href="https://www.hooktheory.com/" target="_blank">Hook Theory</a></li>
+      <li><a href="http://www.piano-e-competition.com/" target="_blank">Yamaha Piano E-Comp</a></li>
+      <li><a href="https://musescore.org/en" target="_blank">Musescore</a></li>
+      <li><a href="https://www.reddit.com/r/datasets/comments/3akhxy/the_largest_midi_collection_on_the_internet/" target="_blank">Midi Man</a></li>
+      <li><a href="https://freemidi.org/genre-dance-eletric" target="_blank">Free Midi</a></li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>Acknowledgements</h3>
     <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="https://www.fast.ai/" target="_blank">Fast.Ai</a></li>
+      <li><a href="https://github.com/wayne391/Lead-Sheet-Dataset" target="_blank">wayne391</a></li>
+      <li><a href="https://github.com/huggingface/" target="_blank">Hugging Face</a></li>
+      <li><a href="https://www.southparkcommons.com/" target="_blank">South Park Commons</a></li>
+      <li><a href="https://www.palapavc.com/" target="_blank">Palapa Ventures</a></li>
     </ul>
   </div>
 </template>
