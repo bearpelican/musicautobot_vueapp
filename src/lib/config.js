fefef77537e5a33ae5020edf16f4575efd7dc8f2
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-04-05 23:30:53 -0700

    Shortening defaultBeat length and saving sequence title

--- src/lib/config.js
+++ src/lib/config.js
@@ -3,6 +3,6 @@ import { getLengthFromValue } from '@/lib/getNotes'
 export const defaultLength = getLengthFromValue(1)
 export const pixelPerBeat = 50
 export const keyWidth = 14
-export const defaultBeats = 200
+export const defaultBeats = 40
 export const startOctave = 2
 export const endOctave = 6
