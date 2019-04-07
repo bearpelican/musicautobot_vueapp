f74e6791d71da4c2172de4b23dc8fb5be09fee52
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-04-07 16:42:44 -0700

    Reverting backend init after history rebase

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ from .client import client_bp
 
 app = Flask(__name__, static_folder='../dist/static')
 app.register_blueprint(api_bp)
-# app.register_blueprint(client_bp)
+app.register_blueprint(client_bp)
 
 from .config import Config
 app.logger.info('>>> {}'.format(Config.FLASK_ENV))
