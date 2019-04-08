ef50f2564ebbb5ff5601114f08602a7b2f8e36df
Andrew Shaw <andrew.t.shaw@gmail.com>
2019-04-08 14:44:19 -0700

    Removing bootstrap. Sticking with vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
-import BootstrapVue from 'bootstrap-vue'
 import VueLodash from 'vue-lodash'
-
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,15 +7,11 @@ import store from './store'
 
 import './filters'
 
-// bootstrap
-import 'bootstrap/dist/css/bootstrap.css'
-
 // vue material
 import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
 
-Vue.use(BootstrapVue)
 Vue.use(Vuetify)
 Vue.use(VueLodash, {})
 
