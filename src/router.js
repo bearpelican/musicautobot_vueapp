b4fbfa6fb34637613ff0f04b1d8403ec4688579a
Andrew Shaw <andrew.t.shaw@gmail.com>
2020-09-11 15:02:43 -0700

    Enabling history mode instead of hash mode

--- src/router.js
+++ src/router.js
@@ -7,7 +7,7 @@ import { shuffleSong } from '@/lib/songPresets'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   // mode: 'history',
   routes: [
     {
@@ -44,5 +44,20 @@ export default new Router({
       component: Sheet
     }
   ],
+  mode: 'history',
   linkActiveClass: 'active' // active class for non-exact links.
+})
+
+export default router
+
+router.beforeEach((to, from, next) => {
+  // Redirect if path begins with a hash (ignore hashes later in path)
+  if (to.redirectedFrom && to.redirectedFrom.substr(0, 2) === '/#') {
+    const path = to.redirectedFrom.substr(2)
+    // eslint-disable-next-line no-console
+    console.log(`Rerouted from ${to.redirectedFrom} to ${path}`)
+    next({ path, replace: true })
+  } else {
+    next()
+  }
 })
